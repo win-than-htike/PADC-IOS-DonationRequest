06855f34f9b24e93cf947bb5b5bb5f150a9723a6
yepyaesonetun <yepyaesone.1995@gmail.com>
2018-12-15 12:56:48 +0630

    initial infra

---
+++ PADC-IOS-DonationRequest/components/CustomButton.swift
@@ -0,0 +1,34 @@
+//
+//  CustomButton.swift
+//  PADC-IOS-DonationRequest
+//
+//  Created by Ye Pyae Sone Tun on 12/15/18.
+//  Copyright © 2018 padcmyanmar. All rights reserved.
+//
+
+import UIKit
+
+class CustomButton: UIButton {
+    @IBInspectable var borderColor: UIColor = UIColor.white {
+        didSet {
+            layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 2.0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 1.0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override public func layoutSubviews() {
+        super.layoutSubviews()
+        clipsToBounds = true
+    }
+}
---
+++ PADC-IOS-DonationRequest/components/ShadowTextField.swift
@@ -0,0 +1,69 @@
+//
+//  ShadowTextField.swift
+//  PADC-IOS-DonationRequest
+//
+//  Created by Ye Pyae Sone Tun on 12/15/18.
+//  Copyright © 2018 padcmyanmar. All rights reserved.
+//
+
+
+import UIKit
+
+class ShadowTextField: UITextField {
+    
+    @IBInspectable var shadowOffsetWidth: Int = 0
+    @IBInspectable var shadowOffsetHeight: Int = 3
+    @IBInspectable var shadowColor: UIColor? = UIColor.lightGray
+    @IBInspectable var shadowOpacity: Float = 0.5
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        addShadow()
+    }
+    
+    func addShadow(){
+        layer.shadowColor = shadowColor?.cgColor
+        layer.shadowOffset = CGSize(width: shadowOffsetWidth, height: shadowOffsetHeight);
+        layer.shadowOpacity = shadowOpacity
+    }
+    
+    // Provides left padding for images
+    override func leftViewRect(forBounds bounds: CGRect) -> CGRect {
+        var textRect = super.leftViewRect(forBounds: bounds)
+        textRect.origin.x += leftPadding
+        return textRect
+    }
+    
+    @IBInspectable var leftImage: UIImage? {
+        didSet {
+            updateLeftView()
+        }
+    }
+    
+    @IBInspectable var leftPadding: CGFloat = 0
+    
+    @IBInspectable var color: UIColor = UIColor.lightGray {
+        didSet {
+            updateLeftView()
+        }
+    }
+    
+    func updateLeftView() {
+        if let image = leftImage {
+            leftViewMode = UITextField.ViewMode.always
+            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
+            imageView.contentMode = .scaleAspectFit
+            imageView.image = image
+            // Note: In order for your image to use the tint color, you have to select the image in the Assets.xcassets and change the "Render As" property to "Template Image".
+            imageView.tintColor = color
+            leftView = imageView
+        } else {
+            leftViewMode = UITextField.ViewMode.never
+            leftView = nil
+        }
+        
+        // Placeholder text color
+        attributedPlaceholder = NSAttributedString(string: placeholder != nil ?  placeholder! : "", attributes:[NSAttributedString.Key.foregroundColor: color])
+    }
+}
